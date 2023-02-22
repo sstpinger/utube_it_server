74b96a1f2523844b44aa0cf04da766b2d70b6b07
sstpinger <115101591+sstpinger@users.noreply.github.com>
2023-02-22 07:22:50 +0500

    Update youtube.com.js

--- pages/api/youtube.com.js
+++ pages/api/youtube.com.js
@@ -1,14 +1,17 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
-  router.get("/", (req, res) => {
-    let url = "https://youtube.com";
-    axios({
-      method: "get",
-      url,
-      responseType: "stream",
-    }).then(function (response) {
-      response.data.pipe(res);
-    });
+  router.get("/", async (req, res) => {
+    let apiUrl = "https://youtube.com";
+
+    console.log(apiUrl);
+    const scratchResponse = await axios.get(apiUrl);
+    try {
+      res.send(scratchResponse);
+      console.log("worked!", apiUrl);
+    } catch (error) {
+      console.log("error in assets get request", apiUrl, error);
+      res.sendStatus(500);
+    }
   });
 }
