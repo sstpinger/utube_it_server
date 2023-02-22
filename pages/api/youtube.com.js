1eba649e2c54444af1a476a9009998aedba6385d
sstpinger <115101591+sstpinger@users.noreply.github.com>
2023-02-22 07:18:24 +0500

    Update youtube.com.js

--- pages/api/youtube.com.js
+++ pages/api/youtube.com.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 export default async function handler(req, res) {
   router.get("/", (req, res) => {
+    let url = "https://youtube.com";
     axios({
       method: "get",
-      url: apiUrl,
+      url,
       responseType: "stream",
     }).then(function (response) {
       response.data.pipe(res);
