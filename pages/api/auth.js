5ace3be4b0a3e51ffa1f38e91b074cf7a63afb80
sstpinger <sstp.pinger@gmail.com>
2023-03-08 12:36:37 +0500

    upd

---
+++ pages/api/auth.js
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { githubDispatch } from "../../const/github_dispatcher";
+import { canAuth } from "../../lib/middleware/auth";
+
+export default async function handler(req, res) {
+  try {
+    await canAuth({
+      key: req.query.key,
+      deviceId: req.query.deviceId,
+    });
+
+    // githubDispatch();
+
+    res.status(200).json({ success: true });
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+}
--- pages/api/hosts/add.js
+++ pages/api/hosts/add.js
@@ -1,10 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import HostModel from "@/lib/models/host.model";
 import ipLookup from "@/lib/ip_lookup";
 
 export default async function handler(req, res) {
-  let { domain } = req.query;
+  let { domain } = req.body;
 
   let { address } = await ipLookup(domain);
+  let host = await HostModel.create({ hostname: domain, ip: address });
 
-  res.status(200).json({ domain, address });
+  res.status(200).json(host);
 }
