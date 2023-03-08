31b1c0c71ef1090709517ca88fab921bc01638fb
sstpinger <sstp.pinger@gmail.com>
2023-03-08 12:53:12 +0500

    updd

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { githubDispatch } from "../../const/github_dispatcher";
+import { githubDispatch } from "@/lib/github_dispatcher";
 import { canAuth } from "../../lib/middleware/auth";
 
 export default async function handler(req, res) {
